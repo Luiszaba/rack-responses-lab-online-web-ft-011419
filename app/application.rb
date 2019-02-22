850e6e2e02a65953868ba95de0d8d9bc52f4a44d
Luis Zabala <luis.zabala076@gmail.com>
2019-02-22 22:31:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,14 @@
 class Application
-  
+
   def call(env)
-    
     resp = Rack::Response.new
-    
-    t = Time.hour
-    
-    if t > 12
-    
-    
-      resp.write "Good Afternoon!"
-    else 
-      resp.write "Good Morning!"
+    now = Time.now
+    if now.hour >=12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
     end
-    
     resp.finish
   end
+
 end
