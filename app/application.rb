dc75a50054d7d7571ea765ac582ee481ed2a2b68
Luis Zabala <luis.zabala076@gmail.com>
2019-02-22 22:24:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    minute = Time.datetime
+    now = Time.now
     
     if Time.now < 12
       resp.write "Good afternoon!"
